1b550b02f1ccac59c00bb0d12ca643c060a87a44
Dario <derioreal63@gmail.com>
2024-06-03 19:25:52 +0200

    test fix

--- src/main/java/de/derioo/action/config/EntryDeserializer.java
+++ src/main/java/de/derioo/action/config/EntryDeserializer.java
@@ -13,10 +13,11 @@ public class EntryDeserializer extends JsonDeserializer<Entry> {
     @Override
     public Entry deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
         JsonNode root = p.getCodec().readTree(p);
-
+        System.out.println(p.getValueAsString());
+        System.out.println(root.toString());
         if (root.has("from") && root.get("from").isTextual() &&
                 root.has("to") && root.get("to").isTextual()) {
-            System.out.println(root.toString());
+
             return ctx.readValue(p, Entry.Simple.class);
         } else {
             return ctx.readValue(p, Entry.class);
