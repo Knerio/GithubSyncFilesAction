8daa727ae7af69fea310441e1484da6edfbc48a6
Dario <derioreal63@gmail.com>
2024-06-03 21:14:51 +0200

    fix

--- src/main/java/de/derioo/action/config/Entry.java
+++ src/main/java/de/derioo/action/config/Entry.java
@@ -13,58 +13,21 @@ import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@JsonDeserialize(using = EntryDeserializer.class)
-public abstract class Entry {
+@Builder
+public class Entry {
 
     Config.SingleFileLocation from;
     Config.SingleFileLocation to;
+
+    @Builder.Default
     @JsonProperty("commit-message")
-    String commitMessage;
-    List<String> ignored;
+    String commitMessage = null;
 
-    @Jacksonized
-    @lombok.Builder
-    @FieldDefaults(level = AccessLevel.PRIVATE)
-    public static class Default extends Entry {
+    @Builder.Default
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    List<String> ignored = new ArrayList<>();
 
-        Config.SingleFileLocation from;
-        Config.SingleFileLocation to;
-
-        @Builder.Default
-        @JsonProperty("commit-message")
-        String commitMessage = null;
-
-        @Builder.Default
-        @JsonIgnoreProperties(ignoreUnknown = true)
-        List<String> ignored = new ArrayList<>();
-
-        public Default(
-                @JsonProperty("from") Config.SingleFileLocation from,
-                @JsonProperty("to") Config.SingleFileLocation to,
-                @JsonProperty("commit-message") String commitMessage,
-                @JsonProperty("ignored") List<String> ignored) {
-            super(from, to, commitMessage, ignored);
-        }
-    }
-
-    @Builder
-    @FieldDefaults(level = AccessLevel.PRIVATE)
-    public static class Simple extends Entry {
-
-        String from;
-        String to;
-
-        public Simple(
-                @JsonProperty("from") String from,
-                @JsonProperty("to") String to) {
-            super(
-                    new Config.SingleFileLocation(System.getenv("INPUT_REPOSITORY"), from),
-                    new Config.SingleFileLocation(System.getenv("INPUT_REPOSITORY"), to),
-                    null,
-                    new ArrayList<>()
-            );
-        }
-    }
 }
