45fafd4d7cbb8e533e2b3fd794a97a7ee6a91ef1
Dario <derioreal63@gmail.com>
2024-06-03 20:07:48 +0200

    add check if contents are the same

--- src/main/java/de/derioo/action/Main.java
+++ src/main/java/de/derioo/action/Main.java
@@ -215,7 +215,17 @@ public class Main {
     @FieldDefaults(level = AccessLevel.PRIVATE)
     private static class ContentsResponse {
 
+        String name;
+        String path;
+        String sha;
+        int size;
+        String url;
+        String html_url;
+        String git_url;
+        String download_url;
+        String type;
         String content;
+        String encoding;
 
     }
 
