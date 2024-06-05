8ac5d0db51b294b094fd9e047e0b9c4ab1082839
Dario <derioreal63@gmail.com>
2024-06-05 14:26:37 +0200

    add new config system

--- src/main/java/de/derioo/action/config/ShortConfig.java
+++ src/main/java/de/derioo/action/config/ShortConfig.java
@@ -1,11 +1,17 @@
 package de.derioo.action.config;
 
 import de.derioo.action.Config;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Jacksonized
+@Builder
+@Getter
 public class ShortConfig extends Config {
     public ShortConfig(@NotNull List<ShortEntry> entries, String globalCommitMessage) {
         super(entries.stream().map(shortEntry -> (Entry) shortEntry).toList(), globalCommitMessage);
@@ -14,14 +20,9 @@ public class ShortConfig extends Config {
     public static class ShortEntry extends Entry {
 
         public ShortEntry(String from, String to) {
-            super(SingleFileLocation
-                    .builder()
-                    .file(from)
-                    .build(),
-                    SingleFileLocation
-                    .builder()
-                    .file(to)
-                    .build(), null, new ArrayList<>());
+            super(new SingleFileLocation(System.getenv("INPUT_REPOSITORY"), from),
+                    new SingleFileLocation(System.getenv("INPUT_REPOSITORY"), to),
+                    null, new ArrayList<>());
         }
     }
 }
